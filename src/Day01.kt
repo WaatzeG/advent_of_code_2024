675ccd0b593ab5cb9929c62043d27dc2c29d2afe
Waatze Goldewijk <waatze.goldewijk@nedap.com>
2024-12-01 22:39:53 +0100

    Remove duplicate code

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,25 +2,13 @@ import kotlin.math.absoluteValue
 
 fun main() {
     fun part1(input: List<String>): Int {
-        val lists = input.flatMap { line ->
-            line.split("   ")
-                .map { it.toInt() }
-        }
-        val firstList = lists.filterIndexed { index, _ -> index % 2 == 0 }.sorted()
-        val secondList = lists.filterIndexed { index, _ -> index % 2 != 0 }.sorted()
-
+        val (firstList, secondList) = getLists(input)
         return firstList.foldIndexed(0) { index: Int, acc: Int, i: Int -> acc + (i - secondList[index]).absoluteValue }
     }
 
     fun part2(input: List<String>): Int {
-        val lists = input.flatMap { line ->
-            line.split("   ")
-                .map { it.toInt() }
-        }
-        val firstList = lists.filterIndexed { index, _ -> index % 2 == 0 }.sorted()
-        val secondList = lists.filterIndexed { index, _ -> index % 2 != 0 }.sorted()
-
-        return firstList.foldIndexed(0) { index: Int, acc: Int, i: Int -> acc + (i * secondList.count { it == i }) }
+        val (firstList, secondList) = getLists(input)
+        return firstList.foldIndexed(0) { _: Int, acc: Int, i: Int -> acc + (i * secondList.count { it == i }) }
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
@@ -37,4 +25,15 @@ fun main() {
         part1(input).println()
         part2(input).println()
     }
+}
+
+fun getLists(input: List<String>): Pair<List<Int>, List<Int>> {
+    val lists = input.flatMap { line ->
+        line.split("   ")
+            .map { it.toInt() }
+    }
+    val firstList = lists.filterIndexed { index, _ -> index % 2 == 0 }.sorted()
+    val secondList = lists.filterIndexed { index, _ -> index % 2 != 0 }.sorted()
+
+    return Pair(firstList, secondList)
 }
